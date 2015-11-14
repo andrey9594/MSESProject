c5305725be5c02c0f4f2314af58740a5fd834f63
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-11-14 16:14:52 +0300

    added working implemetation of getNext()

--- src/main/java/MSES/Project/AgeDataReader.java
+++ src/main/java/MSES/Project/AgeDataReader.java
@@ -6,25 +6,49 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AgeDataReader extends DataReader {
+	private static final Logger log = LoggerFactory.getLogger(AgeDataReader.class);
+	
 	private final Scanner scanner;
 	
 	public AgeDataReader(String fileName) throws IOException {
 		Path path = FileSystems.getDefault().getPath("resources", fileName);
 		scanner = new Scanner(Files.newInputStream(path));
+		scanner.nextLine(); // it's the unused service head information
 	}
 	
 	@Override
-	public Data getNext() {
-		String nextLine = scanner.nextLine();
-		if (nextLine == null)
+	public PopulationData getNext() {
+		try {
+			int year = Integer.parseInt(scanner.next().substring(0, 4)); // some year with the plus symbol at the end
+			/**
+			 * we have some problem with the year 110+
+			 * because the plus at the end hinder our
+			 * parsing process
+			 */
+			int age = -1;
+			String nextAge = scanner.next();
+			if (nextAge.length() == 4)
+				age = Integer.parseInt(nextAge.substring(0, 3));
+			else
+				age = Integer.parseInt(nextAge);
+			double female = Double.parseDouble(scanner.next());
+			double male = Double.parseDouble(scanner.next());
+			scanner.next(); // we don't need the total parameter
+			return new PopulationData(year, age, female, male);
+		} catch(Exception e) {
+			log.warn("Can't parse an information from the file, have we reached the end of the file?");
 			return null;
-		String values[] = nextLine.split(" ");
-		//int year = ;
-		//int age = ;
-		//Data data = new Data();
-		for (String s : values)
-			System.out.println(s);
-		return null;
+		}
+	}
+	
+	@Override
+	public void close() {
+		if (scanner != null) {
+			scanner.close();
+		}
 	}
 }
